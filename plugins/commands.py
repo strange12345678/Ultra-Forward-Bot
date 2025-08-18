151f154a19299be04f3ca3ddbe1d1945e6e32270
strange12345678 <113347850+strange12345678@users.noreply.github.com>
2025-08-18 17:55:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,13 +18,13 @@ from pyrogram import Client, filters, enums, __version__ as pyrogram_version
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaDocument
 
 main_buttons = [[
-        InlineKeyboardButton('📢 Updates', url='https://t.me/Madflix_Bots'),
-        InlineKeyboardButton('💬 Support', url='https://t.me/MadflixBots_Support')
+        InlineKeyboardButton('📢 Updates', url='https://t.me/UniformMovies'),
+        InlineKeyboardButton('💬 Support', url='https://t.me/UniformHelp')
         ],[
         InlineKeyboardButton('🛠️ Help', callback_data='help'),
         InlineKeyboardButton('🩷 About', callback_data='about')
         ],[
-        InlineKeyboardButton('🧑‍💻 Developer 🧑‍💻', url='https://t.me/CallAdminRobot')
+        InlineKeyboardButton('🧑‍💻 Developer 🧑‍💻', url='https://t.me/darkworld008')
         ]]
 
 
