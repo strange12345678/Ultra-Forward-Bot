70631753e09032cad8c5ba1f2500303c55528e8f
strange12345678 <113347850+strange12345678@users.noreply.github.com>
2025-08-18 17:40:34 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -19,7 +19,7 @@ class Translation(object):
   
 ➻ Click Help Button To Know More About Me
   
-<b>Bot Is Made By @Madflix_Bots</b>"""
+<b>Bot Is Made By @DadyIsCalling</b>"""
 
 
   HELP_TXT = """<b><u>🛠️ Help</b></u>
@@ -54,10 +54,10 @@ class Translation(object):
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
-<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a>
-<b>🧑‍💻 Developer :</b> <a href='https://t.me/CallAdminRobot'>Jishu Developer</a>
+<b>📢 Channel :</b> <a href='https://t.me/UniformMovies'>UniformMovies</a>
+<b>🧑‍💻 Developer :</b> <a href='https://t.me/DadyIsCalling'>DadyIsCalling Developer</a>
 
-<b>♻️ Bot Made By :</b> @Madflix_Bots"""
+<b>♻️ Bot Made By :</b> @DadyIsCalling"""
   
   STATUS_TXT = """<b><u>Bot Status</u></b>
   
