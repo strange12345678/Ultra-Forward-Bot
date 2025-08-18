41d9e3d2225c5bc348fbf68c82cf390b0ff03f63
strange12345678 <113347850+strange12345678@users.noreply.github.com>
2025-08-18 18:01:27 +0530

    Update unequify.py

--- plugins/unequify.py
+++ plugins/unequify.py
@@ -19,8 +19,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 CLIENT = CLIENT()
 COMPLETED_BTN = InlineKeyboardMarkup(
    [
-      [InlineKeyboardButton('📢 Updates', url='https://t.me/Madflix_Bots')],
-      [InlineKeyboardButton('💬 Support', url='https://t.me/MadflixBots_Support')]
+      [InlineKeyboardButton('📢 Updates', url='https://t.me/UniformMovies')],
+      [InlineKeyboardButton('💬 Support', url='https://t.me/UniformHelp')]
    ]
 )
 
