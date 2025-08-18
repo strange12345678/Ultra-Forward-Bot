ce24a7fe13e928c545aef7fa9067250539754ebd
strange12345678 <113347850+strange12345678@users.noreply.github.com>
2025-08-18 17:53:12 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -44,9 +44,9 @@ async def get_stats(bot, message):
 
 @Client.on_message(filters.private & filters.command(["donate", "d"]))
 async def donate(client, message):
-	text = "<b>🥲 Thanks For Showing Interest In Donation! ❤️</b> \n\nIf You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice. \n\n<b>🛍 UPI ID:</b> <code>madflixofficial@axl</code>"
+	text = "<b>🥲 Thanks For Showing Interest In Donation! ❤️</b> \n\nIf You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice. \n\n<b>🛍 UPI ID:</b> <code>bibekkumar@fam</code>"
 	keybord = InlineKeyboardMarkup([
-        			[InlineKeyboardButton("🦋 Admin",url = "https://t.me/CallAdminRobot"), 
+        			[InlineKeyboardButton("🦋 Admin",url = "https://t.me/darkworld008"), 
         			InlineKeyboardButton("✖️ Close",callback_data = "close_btn") ]])
 	await message.reply_text(text = text,reply_markup = keybord)
 
